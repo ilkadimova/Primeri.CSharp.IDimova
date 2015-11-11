7b8df1b9459e8c1d77f23760b28ff99971d7b098
ÃÐÐИлка Димова <ilka_i_dimova@abv.bg>
2015-11-11 16:01:09 +0200

    Модул02, видео 04

--- Modul02/Modul02/Program.cs
+++ Modul02/Modul02/Program.cs
@@ -9,10 +9,19 @@ namespace Modul02
 			//Дефиниране на променливи
 			int a = 0, b = 0;
 			int sum = 0;
+			string program_name = "Modul02- Примери";
+			string program_version = "1.0";	
+				
+
+
 			//Събиране на числа
-			a = 5;
-			b = 10;
+			a = 10;
+			b = 5;
 			sum = a + b;
+
+			//За програмата
+			Console.WriteLine(program_name);
+			Console.WriteLine( "Версия: " + program_version+ "\n\n");
 
 			//Писане в конзола
 			Console.Write ( a );
@@ -21,6 +30,29 @@ namespace Modul02
 			Console.Write (" = ");
 			Console.WriteLine ( sum );
 
+
+
+			//Допълнителни оператори
+			Console.WriteLine ("\nИзползване на +=");
+			a += b; //a=10+5, a=a+b
+			Console.WriteLine ( a );
+
+			Console.WriteLine ("\nИзползване на -=");
+			a -= b; //a=15-5, a=a-5
+			Console.WriteLine ( a );
+
+			Console.WriteLine ("\nИзползване на *=");
+			a *= b; //a=10*5, a=a*b
+			Console.WriteLine ( a );
+
+			Console.WriteLine ("\nИзползване на ++");
+			a ++; //a=50+1, a=a+1
+			Console.WriteLine ( a );
+
+			Console.WriteLine ("Стандартно делене:" +(7/3).ToString());
+			Console.WriteLine ("Остатък от делене:" +(7%3).ToString());
+
+		
 
 		}
 	}
